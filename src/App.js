5f1e43d846b89081910aa7550d8cb4e8558bacc4
SachinVarghese <smv@seldon.io>
2019-11-09 11:44:19 +0530

    qutub

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import WarPlane from './art/warplane/WarPlane';
 import Budapest from './art/budapest/Budapest';
+import Qutub from './art/qutub/Qutub';
 
 function App() {
   return (
     <div className="App">
       <WarPlane />
       <Budapest />
+      <Qutub />
     </div>
   );
 }
